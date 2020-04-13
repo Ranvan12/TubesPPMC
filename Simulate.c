2d56deb616ba657c3cc293d5bdbc2661f72e9bd1
Ranvan12 <47746950+Ranvan12@users.noreply.github.com>
2020-04-13 13:54:39 +0700

    Create Simulate.c

---
+++ Simulate.c
@@ -0,0 +1,62 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+ * MODUL 8 – TUGAS BESAR
+ * Kelompok   : 1
+ * Hari dan Tanggal : Senin, 13 April 2020
+ * Asisten (NIM)  : Rahma Rizky Alifia (18316003)
+ * Nama File   : Simulate.c
+ * Deskripsi   : Program berisi prosedur untuk mencari jumlah neighbor dan
+ *               menyimulasikan Game of Life untuk 1 iterasi
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct cell {
+	char data;
+	int neighbor;
+} cell;
+
+//Prosedur untuk mengisi data neighbor
+void AliveNeighbor (cell*** A, int r, int c){
+	int i, j, h, v;
+	int row, column;
+	for (i = 0; i < r; i = i + 1){
+		for (j = 0; j < c; j = j + 1){
+			(*A)[i][j].neighbor = 0;
+			for (h = -1; h <= 1; h = h + 1){
+				for (v = -1; v <= 1; v = v + 1){
+					row = (i + h + r) % r;
+					column = (j + v + c) % c;
+					if (((row != i) || (column != j)) && ((*A)[row][column].data == 'X')){
+						 (*A)[i][j].neighbor = (*A)[i][j].neighbor + 1;
+					}
+				}
+			}
+		}
+	}
+}
+
+//Prosedur untuk menyimulasikan dalam satu iterasi
+void Update (cell*** A, int r, int c){
+	int i, j;
+	AliveNeighbor (A, r, c);
+	for (i = 0; i < r; i = i + 1){
+		for (j = 0; j < c; j = j + 1){
+			if (((*A)[i][j].data == 'X') && (((*A)[i][j].neighbor <= 1) || ((*A)[i][j].neighbor >= 4))){
+				(*A)[i][j].data = '-';
+			}
+			else if (((*A)[i][j].data == '-') && ((*A)[i][j].neighbor == 3)){
+				(*A)[i][j].data = 'X';
+			}
+		}
+	}
+}
+
+/* Note: Prosedur di atas sudah dicoba dan berhasil untuk input array 5 x 5 berisi:
+          - - - - -
+          - - X - -
+          - X X X -
+          - - X - -
+          - - - - -
+         selama 3 iterasi
+ */
