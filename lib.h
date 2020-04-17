94cbc5a8793a663c1509c47e93a39f6b484bde60
tifanysaulina <43534527+tifanysaulina@users.noreply.github.com>
2020-04-17 11:47:16 +0700

    Update lib.h

--- lib.h
+++ lib.h
@@ -15,14 +15,14 @@
 
 //#include "input_file.c"
 #include "Simulate.c"
-#include "display.c"
+#include "Display.c"
 
 typedef struct cell {
 	char data;
 	int neighbor;
 } cell;
 
-//void input_file(cell*** matrix, int *r, int *c, char *filename);
+//void input_file(cell*** matrix, int *r, int *c);
 void AliveNeighbor (cell*** A, int r, int c);
 void Update (cell*** A, int r, int c);
 void Delay(int time);
